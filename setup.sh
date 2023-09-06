a3882ee030d7f50fb882836bae7491aba014c90e
Bruno Souza <bsouza.dev@gmail.com>
2023-09-05 21:40:34 -0300

    SRE-7 Improving binary installation

--- setup.sh
+++ setup.sh
@@ -4,21 +4,27 @@ set -v # show current command
 
 mkdir $GITHUB_WORKSPACE/bin
 
-echo "KUBE_VERSION: $KUBE_VERSION"
+[[ -n "${KUBE_VERSION}" ]] && KV=$KUBE_VERSION || KV=$(curl -L -s https://dl.k8s.io/release/stable.txt | cut -d'v' -f2)
+[[ -n "${AWS_IAM_AUTH_VERSION}" ]] && AIAV=$AWS_IAM_AUTH_VERSION || AIAV="0.6.11" #Based on https://github.com/kubernetes-sigs/aws-iam-authenticator/tags
 
 # kubectl
-curl -LO "https://dl.k8s.io/release/$KUBE_VERSION/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$KUBE_VERSION/bin/linux/amd64/kubectl.sha256"
+echo "Installing kubectl version: v${KV}"
+curl -LO "https://dl.k8s.io/release/v${KV}/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/v${KV}/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 chmod +x kubectl
 
 # aws-iam-authenticator
-curl -LO "https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator"
+echo "Installing aws-iam-authenticator version: v${AIAV}"
+curl -Lo aws-iam-authenticator_${AIAV}_linux_amd64 https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${AIAV}/aws-iam-authenticator_${AIAV}_linux_amd64
+curl -Lo aws-iam-authenticator.txt https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${AIAV}/authenticator_${AIAV}_checksums.txt
+echo -n "$(egrep "aws-iam-authenticator_${AIAV}_linux_amd64" aws-iam-authenticator.txt | cut -d' ' -f1)  aws-iam-authenticator_${AIAV}_linux_amd64" | sha256sum --check \
+	&& mv aws-iam-authenticator_${AIAV}_linux_amd64 aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
 # moving
 mv kubectl /usr/local/bin/kubectl
-mv aws-iam-authenticator $GITHUB_WORKSPACE/bin
+mv aws-iam-authenticator $GITHUB_WORKSPACE/bin/aws-iam-authenticator
 
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
 
